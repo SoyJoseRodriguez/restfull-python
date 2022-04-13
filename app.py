1ef571856b80d59b32e2622b5dd8c02e9af0ad9e
Jose Rodriguez <josergzruiz@outlook.es>
2022-04-13 16:28:52 -0500

    Comentarios

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
+"""
+    ! ---- Importación de librerías ---- 
+    @param flask: Trabajar con el framework Web y todos los beneficios 
+"""
 from flask import Flask
-from flask_cors import CORS
+#from flask_cors import CORS
+
+"""
+    ! ---- Importación de Archivos ---- 
+    @param database: Carpeta donde se encuentra todo lo relacionado a la base de datos
+    @param setup: Archivo donde están todas las configuraciones del proyecto
+    @param resources.tasks: Carpeta y Archivo donde se encuentran todos los metodos a usar  
+    @param tasks_bd: Base de datos que almacenara la información de las tareas 
+"""
 
 from database import setup
 from resources.tasks import tasks_bp
 
-app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
-setup.create_tables()
+app = Flask(__name__) # <-- el objeto con el que trabajaremos en el proyecto
+#cors = CORS(app, resources={r"/*": {"origins": "*"}})
+setup.create_tables() # <-- de el archivo de configuración crearemos la tabla tasks con la que estaremos trabajando
 
 
-@app.before_first_request
+@app.before_first_request # <-- Trabajar con los request de 
 def create_tables():
     setup.create_tables()
 
--- resources/tasks.py
+++ resources/tasks.py
@@ -1,60 +1,70 @@
+"""
+    ! ---- Importación de librerías ---- 
+    @param flask: Trabajar con el framework Web y todos los beneficios 
+    @param request: Trabajamos con solicitudes en la web
+    @param jsonify: Trabajamos con formato JSON
+    @param Blueprint: Trabajar con el diseño y arquitectura de flask
+    @param datetime: Tendremos la fecha del sistema en diferentes formatos 
+"""
+
 from flask import request, jsonify, Blueprint
 from datetime import datetime
 
 from database import tasks
 
 
-tasks_bp = Blueprint('routes-tasks', __name__)
+tasks_bp = Blueprint('routes-tasks', __name__) # <-- Dar diseño a la ruta
 
-@tasks_bp.route('/tasks', methods=['POST'])
-def add_task():
-    title = request.json['title']
-    created_date = datetime.now().strftime("%x") # 5/22/2021
+@tasks_bp.route('/tasks', methods=['POST']) # <-- Trabajar con POST
+def add_task(): # <-- Registrar tarea
+    title = request.json['title'] # <-- Pasar por parametro JSON el titulo de la tarea
+    created_date = datetime.now().strftime("%x") # <-- Da formato de fecha tipo 5/13/2022
 
-    data = (title, created_date)
-    task_id = tasks.insert_task(data)
+    data = (title, created_date) # <-- Crear el titulo y fecha
+    task_id = tasks.insert_task(data) # <-- Insertar la tarea por id
 
     if task_id:
-        task = tasks.select_task_by_id(task_id)
+        task = tasks.select_task_by_id(task_id) # <-- Si existe la tarea no se agregara
         return jsonify(task)
+
     return jsonify({'mensaje': 'Error Interno'})
 
 
-@tasks_bp.route('/tasks', methods=['GET'])
-def get_tasks():
-    data = tasks.select_all_tasks()
+@tasks_bp.route('/tasks', methods=['GET']) # <-- Trabajar con GET
+def get_tasks(): # <-- Obtener tareas 
+    data = tasks.select_all_tasks() # <-- seleccionamos todas las tareas
 
     if data:
-        return jsonify({'tasks': data})
+        return jsonify({'tasks': data}) # <-- Enseñamos las tareas
     elif data == False:
-        return jsonify({'mensaje': 'Error Interno'})
+        return jsonify({'mensaje': 'Error Interno'}) # <-- Mostramos en caso de error
     else:
-        return jsonify({'tasks': {}})
+        return jsonify({'tasks': {}}) # <-- Mostramos la información de las tareas
 
-@tasks_bp.route('/tasks', methods=['PUT'])
-def update_task():
-    title = request.json['title']
-    id_arg = request.args.get('id')
+@tasks_bp.route('/tasks', methods=['PUT']) # <-- Trabajar con PUT
+def update_task(): # <-- Actualizar tarea
+    title = request.json['title'] # <-- Pasamos el titulo de la tarea
+    id_arg = request.args.get('id') # <-- Pasamos el id de la tarea a actualizar
 
-    if tasks.update_task(id_arg, (title,)):
-        task = tasks.select_task_by_id(id_arg)
+    if tasks.update_task(id_arg, (title,)): 
+        task = tasks.select_task_by_id(id_arg) # <-- Actualizamos la tarea por el id
         return jsonify(task)
-    return jsonify({'mensaje': 'Error Interno'})
+    return jsonify({'mensaje': 'Error Interno'}) # <-- En caso de error mostramos
 
-@tasks_bp.route('/tasks', methods=['DELETE'])
-def delete_task():
-    id_arg = request.args.get('id')
+@tasks_bp.route('/tasks', methods=['DELETE']) # <-- Trabajamos con DELETE
+def delete_task(): # <-- Eliminar tarea
+    id_arg = request.args.get('id') # <-- Pasamos el id de la tarea a eliminar
 
     if tasks.delete_task(id_arg):
-        return jsonify({'mensaje': 'Tarea Eliminada'})
-    return jsonify({'mensaje': 'Error Interno'})
+        return jsonify({'mensaje': 'Tarea Eliminada'}) # <-- Mostramos la tarea eliminada
+    return jsonify({'mensaje': 'Error Interno'}) # <-- En caso de error mostramos
 
 
-@tasks_bp.route('/tasks/completed', methods=['PUT'])
-def complete_task():
-    id_arg = request.args.get('id')
-    completed = request.args.get('completed')
+@tasks_bp.route('/tasks/completed', methods=['PUT']) # Trabajamos con PUT
+def complete_task(): # <-- Completamos tarea
+    id_arg = request.args.get('id') # <-- Pasamos el id de la tarea
+    completed = request.args.get('completed') # <-- Pasamos tarea completada
 
     if tasks.complete_task(id_arg, completed):
-        return jsonify({'mensaje': 'Succesfully'})
-    return jsonify({'mensaje': 'Error Interno'})
+        return jsonify({'mensaje': 'Tarea completada'}) # <-- Mostramos tarea completada
+    return jsonify({'mensaje': 'Error Interno'}) # <-- En caso de error mostramos 
