792c9aa5805d4f580bcc0880ff80b9a713945f35
Jose Rodriguez <josergzruiz@outlook.es>
2022-04-13 16:33:05 -0500

    Comentarios

--- database/connection.py
+++ database/connection.py
@@ -1,12 +1,18 @@
+"""
+    ! ---- Importación de librerías ---- 
+    @param sqlite3: Trabajar con el gestor de bases de datos
+    @param Error: Trabajar con los errores que se presenten en el gestor SQLite
+"""
+
 import sqlite3
 from sqlite3 import Error
 
 
-def create_connection():
-    conn = None
+def create_connection(): # <-- Para crear la conexion de la base de datos
+    conn = None # <-- Crear la bariable conexión
 
     try:
-        conn = sqlite3.connect("database/tasks.db")
+        conn = sqlite3.connect("database/tasks.db") # <-- Crear la conexión 
     except Error as e:
-        print(f"Error al conectar a la base de datos {e}")
+        print(f"Error al conectar a la base de datos {e}") # <-- Mostrar en caso de error
     return conn
