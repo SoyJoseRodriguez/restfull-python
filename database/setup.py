710ec055c5e570774267e8019fb9b8a966d4eda6
Jose Rodriguez <josergzruiz@outlook.es>
2022-04-13 16:38:50 -0500

    Comentarios

--- database/setup.py
+++ database/setup.py
@@ -1,29 +1,42 @@
+"""
+    ! ---- Importación de librerías ---- 
+    @param sqlite3: Trabajar con el gestor de bases de datos
+    @param Error: Trabajar con los errores que se presenten en el gestor SQLite
+"""
+
 import sqlite3
 from sqlite3 import Error
+
+"""
+    ! ---- Trabajar con archivos ---- 
+    @param connection: Trabajar con el archivo de connection
+    @param create_connection: Trabajar con la función de conexión a la base de datos
+"""
 
 from .connection import create_connection
 
 
-def read_file(path):
-    with open(path, "r") as sql_file:
+def read_file(path): # <-- Para leer un archivo
+    with open(path, "r") as sql_file: # <-- Para abrir el archivo
         return sql_file.read()
 
-def create_tables():
-    conn = create_connection()
+def create_tables(): # <-- Para crear las tablas
+    conn = create_connection() # <-- crear la conexión 
 
-    path = "database/sql/tables.sql"
+    path = "database/sql/tables.sql" # <-- Ruta para leer el archivo
 
-    sql = read_file(path)
+    sql = read_file(path) # <-- Leer el archivo
   
     try:
-        cur = conn.cursor()
-        cur.execute(sql)
-        conn.commit()
+        cur = conn.cursor() # <-- Crear el cursor
+        cur.execute(sql) # <-- ejecutar sql
+        conn.commit() # <-- verificar el sql
         return True
+
     except Error as e:
-        print(f"Error al crear la tabla: {e}")
+        print(f"Error al crear la tabla: {e}") # <-- Mostrar en caso de error
         return False
     finally:
         if conn:
-            cur.close()
-            conn.close()
+            cur.close() # <-- cerrar cursor
+            conn.close() # <-- cerrar conexión 
